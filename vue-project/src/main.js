5c0a4e003503fa7edc5a29c11141940384d8822b
nasihaassakinah <nanasihaasakinah@gmail.com>
2025-05-09 14:58:23 +0700

    pratikum_6

---
+++ vue-project/src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'animate.css'
+import 'aos/dist/aos.css'
+import './assets/global.css'
+
+import AOS from 'aos'
+
+createApp(App).use(router).mount('#app')
+
+AOS.init({ duration: 800, once: true })
